024349259d94333bb1ed7a1c797c8082a648665e
Jonathan Mitchell <jonathan@mugginsoft.com>
2014-09-12 17:40:35 +0100

    Restored margin

--- Source/PSMTabBarControl.h
+++ Source/PSMTabBarControl.h
@@ -17,8 +17,7 @@
 
 #define kPSMTabBarControlHeight             22
 // internal cell border
-#warning MARGIN_X changed from 6. Proper implementation required.
-#define MARGIN_X							16 // interior cell width calculated at - (CGFloat)_minimumWidthOfCell - (CGFloat)_desiredWidthOfCell
+#define MARGIN_X							6 // interior cell width calculated at - (CGFloat)_minimumWidthOfCell - (CGFloat)_desiredWidthOfCell
 #define MARGIN_Y							3
 // padding between objects
 #define kPSMTabBarCellPadding				4
